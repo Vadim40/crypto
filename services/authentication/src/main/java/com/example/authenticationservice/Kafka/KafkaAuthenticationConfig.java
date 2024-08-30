701586377d9eaa39a45373a16a2eb9cf0320707f
Vadim <vadimitrius@gmail.com>
2024-08-30 14:45:17 +0300

    fixed KafkaAuthenticationTopicConfig

---
+++ services/authentication/src/main/java/com/example/authenticationservice/Kafka/KafkaAuthenticationConfig.java
@@ -0,0 +1,30 @@
+package com.example.authenticationservice.Kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaAuthenticationConfig {
+
+    @Bean
+    public NewTopic accountCreationTopic() {
+        return TopicBuilder.name("account-creation").build();
+    }
+
+    @Bean
+    public NewTopic userLoginEventsTopic() {
+        return TopicBuilder.name("user-login-events").build();
+    }
+
+    @Bean
+    public NewTopic passwordChangeEventsTopic() {
+        return TopicBuilder.name("password-change-events").build();
+    }
+
+    @Bean
+    public NewTopic otpVerificationTopic() {
+        return TopicBuilder.name("otp-verification").build();
+    }
+}
