f6c16dfe9b7910e9cdddf6b45061c90276cfb1f5
Vadim <vadimitrius@gmail.com>
2024-09-08 15:43:21 +0300

    temporary changes

--- services/authentication/src/main/java/com/example/authenticationservice/Kafka/KafkaProducerConfig.java
+++ services/authentication/src/main/java/com/example/authenticationservice/Kafka/KafkaProducerConfig.java
@@ -49,7 +49,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, PasswordChangeEvent> passwordChangeEventProducerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
