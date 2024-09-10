52d1f830f40d9cbbc8d2b00941eb14068de54dad
Vadim <vadimitrius@gmail.com>
2024-09-10 14:29:29 +0300

    added Redis to store email:id info

--- services/wallet/src/main/java/com/example/wallet/Services/AccountServiceImpl.java
+++ services/wallet/src/main/java/com/example/wallet/Services/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.wallet.Services;
 import com.example.wallet.Client.AuthenticationClient;
 import com.example.wallet.Models.DTO.AccountResponse;
 import com.example.wallet.Services.Interfaces.AccountService;
+import com.example.wallet.Services.Interfaces.RedisAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AccountServiceImpl implements AccountService {
     private final AuthenticationClient authenticationClient;
+    private final RedisAccountService redisAccountService;
 
     @Override
-    public AccountResponse findAccountByEmail(String email) {
-        return authenticationClient.findAccountByEmail(email);
+    public Long findAccountIdByEmail(String email) {
+        return redisAccountService.getAccountId(email).orElseGet(() -> {
+            AccountResponse accountResponse = authenticationClient.findAccountByEmail(email);
+            Long accountId = accountResponse.id();
+            redisAccountService.saveAccount(accountResponse);
+            return accountId;
+        });
     }
+
 
 
 }
